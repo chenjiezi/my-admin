47a4e6303fda96c386ea969f2f9f6adc54cb03a7
chenjiezi <892774940@qq.com>
2021-03-22 14:24:53 +0800

    引入iviewui、axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+
+import { Button, Table, Input } from 'view-design';
+Vue.component('Button', Button);
+Vue.component('Table', Table);
+Vue.component('Input', Input);
 
 new Vue({
   router,
