32e70abc76f9e0e4ba4be8e11d8ecd1a7bc062e6
chenjiezi <892774940@qq.com>
2021-03-23 23:05:45 +0800

    封装 login\logout 数据接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,20 @@
 import request from '@/utils/request'
 
 export function login (data) {
-  return request({
-    url: '',
-    method: 'post',
-    data
+  console.log('data:', data)
+  const { username, password } = data;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username === 'admin' && password === 'admin') {
+        resolve({ code: 200, data: { token: 'token' }, message: '登录成功' });
+      } else {
+        reject({ code: 200, data: null, message: '账号或密码错误~~~~' });
+      }
+    }, 888);
   })
 }
 
 export function logout () {
-  return request({
-    url: '',
-    method: 'post'
-  })
+  return Promise.resolve({ code: 200 });
 }
