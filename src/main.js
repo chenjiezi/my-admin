1af5e085523464a00687d98d2d37d47d6ef817a3
chenjiezi <892774940@qq.com>
2021-03-23 23:05:14 +0800

    按需加载iview - form|icon|message

--- src/main.js
+++ src/main.js
@@ -6,10 +6,24 @@ import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false
 
-import { Button, Table, Input } from 'view-design';
+import {
+  Button,
+  Table,
+  Input,
+  Form,
+  FormItem,
+  Icon,
+  Message
+} from 'view-design';
+
 Vue.component('Button', Button);
 Vue.component('Table', Table);
 Vue.component('Input', Input);
+Vue.component('Form', Form);
+Vue.component('FormItem', FormItem);
+Vue.component('Icon', Icon);
+
+Vue.prototype.$Message = Message
 
 new Vue({
   router,
